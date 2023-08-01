d0390de57f0359b3d1aa35bb1ca0f6382d26aedf
patilajayv <90689474+patilajayv@users.noreply.github.com>
2023-08-02 00:28:18 +0530

    adding area of the circle Feature

--- GeometryCalculator.py
+++ GeometryCalculator.py
@@ -16,9 +16,10 @@ if __name__== "main":
     calculator  = GeometryCalculator()
 
 
-# TODO: Implement the feature to calculate the area of a circle # radius = 5
+# TODO: Implement the feature to calculate the area of a circle # 
+    radius = 5
 
-# print(f"The area of the circle with radius {radius} ={calculator.calculate_circle_area(radius)}")
+    print(f"The area of the circle with radius {radius} ={calculator.calculate_circle_area(radius)}")
 
  
 
